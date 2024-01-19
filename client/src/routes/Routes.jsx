b57f9b0deb22299e6f7753ee549fca5e97717a22
unknown <you@example.com>
2024-01-19 11:45:16 +0600

    Add category icons and name

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import Main from '../layouts/Main'
+import Home from '../Pages/Home'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Main />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
   },
 ])
